c9c343a7bc5160692554e30c75ae560409b8d6ef
Katerina Kossler <katerinakossler@gmail.com>
2020-01-14 20:40:40 -0800

    basic data structure for web app

--- model.py
+++ model.py
@@ -1,90 +1,179 @@
+import enum
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-### Classes to add: 
+# Need to add references between tables 
 
-# Table users as U {
-#   id long [pk, increment] // auto-increment
-#   username varchar
-#   email varchar
-#   password varchar // hash their pasword and temp store hash
-#   created_at timestamp
-#   first_name varchar [not null]
-#   last_name varchar [not null]
-#   bio varchar
-#   picture_url varchar
-#   zipcode int
-#   canceled_by_user boolean
-# }
 
-# Enum goal_type {
-#   number_hikes
-#   miles_hiked
-#   feet_ascended
-#   hikeable_miles [note: 'miles in one hike']
-#   hike_difficulty
-# }
+class User(db.model):
+    """Users of the web app"""
+    __tablename__ = 'users'
 
-# Enum progress {
-#   not_started
-#   in_progress
-#   complete
-# }
+    user_id = db.Column(db.BigInteger(), 
+                        autoincrement=True, 
+                        primary_key=True)
+    username = db.Column(db.String(128),
+                         nullable=False)
+    email = db.Column(db.String(256), 
+                      nullable=False)
+    password = db.Column(db.String(64), 
+                         nullable=False) # hash with salt
+    created_at = db.Column(db.DateTime(), 
+                           nullable=False)
+    first_name = db.Column(db.String(64), 
+                           nullable=False)
+    last_name = db.Column(db.String(64), 
+                          nullable=False)
+    bio = db.Column(db.String(1024),
+                    nullable=True)
+    image_url = db.Column(db.String(1024)
+                          nullable=True)
+    zipcode = db.Column(db.String(10),
+                        nullable=True)
+    canceled_by_user = db.Column(db.Boolean(), 
+                                 nullable=False)
 
-# Table goals {
-#   id long [pk, increment] // auto-increment
-#   user_id long [ref: > U.id] 
-#   title varchar
-#   goal goal_type
-#   numerical_value float
-#   units varchar
-#   description varchar
-#   goal_start_on datetime 
-#   goal_end_on datetime
-#   status status
-#   canceled_by_user boolean
-# }
+class GoalType(enum.Enum):
+    number_hikes = 1
+    miles_hiked = 2
+    feet_ascended = 3
+    hikeable_miles = 4
+    hike_difficulty = 5
 
-# Table trails  [headercolor: #27ae60] {
-#   id long [pk, increment] // auto-increment
-#   trail_name varchar
-#   description text
-#   difficulty varchar
-#   distance float // in miles
-#   total_ascent int
-#   total_descent int
-#   location varchar
-#   lat float
-#   long float
-#   rating int
-# }
+class Progress(enum.Enum):
+    not_started = 1
+    in_progress = 2
+    complete = 3
 
-# Table trail_status  [headercolor: #27ae60] {
-#   id long [pk, ref: - trails.id]
-#   status varchar // unk, all clear, variety of issues
-#   description varchar
-#   status_at timestamp
-# }
+class Goal(db.model):
+    """General; hiking related goals"""
+    __tablename__ = 'goals'
 
-# Table hikes  [headercolor: #27ae60] {
-#   id long [pk, increment] // auto-increment
-#   user_id long [ref: > U.id]
-#   trail_id long [ref: > trails.id]
-#   status varchar // 
-#   details varchar 
-#   hiked_on datetime [default: 'now()']
-#   cenceled_by_user boolean
-# }
+    goal_id = db.Column(db.BigInteger(), 
+                        autoincrement=True, 
+                        primary_key=True,
+                        nullable=False)
+    user_id = db.Column(db.BigInteger(), 
+                        db.ForeignKey('users.user_id'),
+                        nullable=False)
+    title = db.Column(db.String(1024),
+                      nullable=False)
+    goal = db.Column(Enum(GoalType),
+                     nullable=False)
+    numerical_value = db.Column(db.Float(),
+                                nullable=False)
+    units = db.Column(db.String(32),
+                      nullable=False)
+    description = db.Column(db.String(512),
+                            nullable=True)
+    goal_start_on = db.Column(db.DateTime(),
+                              nullable=False) 
+    goal_end_on = db.Column(db.DateTime(),
+                            nullable=False)
+    status = db.Column(Enum(Progress),
+                       nullable=False)
+    canceled_by_user = db.Column(db.Boolean(), 
+                                 nullable=False)
 
-# Table hike_results  [headercolor: #27ae60] {
-#   id long [pk, increment] // auto-increment
-#   hike_id long [ref: - hikes.id]
-#   description text
-#   ascent_rating int
-#   distance_rating int
-#   challenge_rating int
-#   hike_time float // hrs
-#   image_url varchar // url to an image
-#   canceled_by_user boolean
-# }
+class Trail(db.model):
+    """Trail identifying information"""
+    __tablename__ = 'trails'
+
+    trail_id = db.Column(db.BigInteger(), 
+                        autoincrement=True, 
+                        primary_key=True,
+                        nullable=False)
+    trail_name = db.Column(db.String(256), 
+                           nullable=False)
+    description = db.Column(db.String(1024),
+                            nullable=False)
+    difficulty = db.Column(db.String(64),
+                           nullable=False)
+    distance_in_miles = db.Column(db.Float(),
+                                  nullable=False)
+    total_ascent = db.Column(db.Integer(),
+                             nullable=True)
+    total_descent = db.Column(db.Integer(),
+                              nullable=True)
+    location = db.Column(db.String(256),
+                         nullable=False) 
+    latitude = db.Column(db.Float(),
+                         nullable=False)
+    longitude = db.Column(db.Float(),
+                          nullable=False)
+    rating = db.Column(db.Float(),
+                       nullable=True)
+
+class TrailStatus(db.model):
+    """Give trail status (quality / safety) information by trail id"""
+    __tablename__ = 'status_of_trails'
+
+    trail_id = db.Column(db.BigInteger(), 
+                         autoincrement=True,
+                         db.ForeignKey('status_of_trails.trail_id'), 
+                         primary_key=True,
+                         nullable=False)
+    status = db.Column(db.String(256),
+                       nullable=False)
+    description = db.Column(db.String(1024),
+                            nullable=False)
+    status_at = db.Column(db.DateTime(),
+                          nullable=False)
+
+class Hike(db.model):
+    """An instance of going on a trail of movement activity"""
+    __tablename__ = 'hikes'
+    
+    hike_id = db.Column(db.BigInteger(), 
+                        autoincrement=True, 
+                        primary_key=True,
+                        nullable=False)
+    user_id = db.Column(db.BigInteger(), 
+                        db.ForeignKey('users.user_id'),
+                        nullable=False)
+    trail_id = db.Column(db.BigInteger(), 
+                         db.ForeignKey('trails.trail_id'),
+                         nullable=True)
+    status = db.Column(Enum(Progress),
+                       nullable=False)
+    details = db.Column(db.String(1024),
+                        nullable=True)
+    hiked_on = db.Column(db.DateTime(),
+                         nullable=True)
+    canceled_by_user = db.Column(db.Boolean(), 
+                                 nullable=False)
+
+class ResultRating(enum.Enum):
+    very_easy = 1
+    easy = 2
+    average = 3
+    difficult = 4
+    very_difficult = 5
+
+class HikeResult(db.model):
+    """Results of a hike instance"""
+    __tablename__ = 'hike_results'
+
+    result_id = db.Column(db.BigInteger(), 
+                        autoincrement=True, 
+                        primary_key=True,
+                        nullable=False)
+    hike_id = db.Column(db.BigInteger(), 
+                        autoincrement=True, 
+                        db.ForeignKey('hikes.hike_id'),
+                        nullable=False)
+    description = db.Column(db.String(1024),
+                            nullable=True)
+    ascent_rating = db.Column(Enum(HikeResult),
+                              nullable=True)
+    distance_rating = db.Column(Enum(HikeResult),
+                                nullable=False)
+    challenge_rating = db.Column(Enum(HikeResult),
+                                 nullable=False)
+    hike_time = db.Column(db.Float(),
+                          nullable=False)
+    image_url = db.Column(db.String(1024)
+                          nullable=True)
+    cenceled_by_user = db.Column(db.Boolean(), 
+                                 nullable=False)
