041387237cd9890860837afaa4f0bba603d5079d
katerina-kossler <katerinakossler@gmail.com>
2020-02-07 06:45:25 +0000

    Added successfull API calls for trail search

--- server.py
+++ server.py
@@ -9,8 +9,10 @@ import re
 import os
 import pgeocode
 import requests
-
+ 
+key = os.environ["HIKINGPROJECT_KEY"]
 coordinates = pgeocode.Nominatim('us')
+
 app = Flask(__name__)
 
 # A secret key is needed to use Flask sessioning features
@@ -37,25 +39,29 @@ def authenticate_user():
     password = request.form.get("password")
 
     user_in_system = User.query.filter_by(username=username).first()
+    email_in_system = User.query.filter_by(email=email).first()
+
     if user_in_system:
         user_password = user_in_system.password
-
-    email_in_system = User.query.filter_by(email=email).first()
+        if (user_password == password):
+            session['current_user'] = user_in_system.user_id
+            flash('Successfully Logged in')
+            return redirect('/profile')
+        else:
+            flash('Incorrect login information; try again')
+            return redirect('/login')
     if email_in_system:
         email_password = email_in_system.password
-
-    if (user_password == password) or (email_password == password):
-        if user_in_system:
-            session['current_user'] = user_in_system.user_id
-        else:
+        if (email_password == password):
             session['current_user'] = email_in_system.user_id
+            flash('Successfully Logged in')
+            return redirect('/profile')
+        else:
+            flash('Incorrect login information; try again')
+            return redirect('/login')
 
-        flash('Successfully Logged in')
-        return redirect('/profile')
-
-    else:
-        flash('Incorrect login information; try again')
-        return redirect('/login')
+    flash('Incorrect login information; try again')
+    return redirect('/login')
 
 @app.route("/logout", methods=["GET"])
 def logout():
@@ -85,8 +91,8 @@ def intake_user_info():
     created_on = date.today()
     first_name = request.form.get("first")
     last_name = request.form.get("last")
-    image_url = request.form.get("url")
-
+    image_url = "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+    # might give option for radio buttons of profile image_url if time
     username_in_system = User.query.filter_by(username=username).first()
     email_in_system = User.query.filter_by(email=email).first()
 
@@ -101,10 +107,6 @@ def intake_user_info():
     elif email_in_system and email_in_system.canceled_by_user == False:
         flash("There is already an active account with that email!")
         return redirect("/register")
-    else:
-        if image_url == "https://example.com":
-            image_url = "https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-# if time, have a better system or use tool to show user a preview of their profile picture
 
         user = User(username=username,
                     email=email,
@@ -132,31 +134,30 @@ def show_profile():
     print(goals)
     return render_template("profile.html", user=user, goals=goals)
 
-def edit_profile():
-    """give user the option to edit their information"""
-    user_id = session["current_user"]
-    user = User.query.get(user_id)
+# def edit_profile():
+#     """give user the option to edit their information"""
+#     user_id = session["current_user"]
+#     user = User.query.get(user_id)
 
-    return render_template("edit-profile.html", user=user)
-    # still in progress
+#     return render_template("edit-profile.html", user=user)
+#     # still in progress
 
-@app.route("/profile", methods=["POST"])
-def update_profile():
-    """Updates a registered, active user's profile information"""
-    pass
+# @app.route("/profile", methods=["POST"])
+# def update_profile():
+#     """Updates a registered, active user's profile information"""
+#     pass
 
 @app.route("/trails", methods=["GET"])
 def show_search_form():
     """Shows user a search form for a trail"""
-
-    return render_template("trails.html")
+    if not 'current_user' in session:
+        return redirect('/')
+    return render_template("trails.html", trails=None)
 
 
 @app.route("/trails", methods=["POST"])
 def load_search_results():
     """shows results from API for trails available given specifications"""
-
-
     zipcode = request.form.get("zipcode")
 
     details = coordinates.query_postal_code(zipcode)
@@ -167,19 +168,19 @@ def load_search_results():
     min_length = request.form.get("length")
     sort = request.form.get("sort")
     max_results = request.form.get("max_results")
+    payload = {"key":key,
+               "lon":longitude,
+               "lat":latitude,
+               "maxDistance":search_distance,
+               "minLength":min_length,
+               "sort":sort,
+               "maxResults":max_results}
 
-    pass
+    r_trails = requests.get("https://www.hikingproject.com/data/get-trails", params=payload)
+    j_trails = r_trails.json()
+    trails = j_trails['trails']
 
-@app.route("/goals", methods=["GET"])
-def show_current_goals_and_progress():
-    """View active goals and see graphs for current progress towards the goal"""
-    pass
-
-
-@app.route("/goals", methods=["POST"])
-def change_goals():
-    """Add, modify, or cancel a selected, active goal"""
-    pass
+    return render_template("trails.html", trails=trails)
 
 @app.route("/hikes", methods=["GET"])
 def show_current_hikes():
@@ -187,27 +188,65 @@ def show_current_hikes():
         - status of each hike
         - links to edit status
         - link to add/ modify results if complete"""
-
-    pass
+    if not 'current_user' in session:
+        return redirect('/')
+    
+    user_id = session["current_user"]
+    hikes = Hike.query.filter_by(user_id=user_id).all()
+    # should join hikes and trails tables to see / link to the t
+    return render_template("hikes.html", hikes=hikes)
 
 @app.route("/hikes", methods=["POST"])
 def add_hike():
     """Add a trail to a list of hikes the user wants to go on;
        if trail is already in hikes and is not complete display an alert that
        hike is already in list"""
-    pass
 
-@app.route("/results", methods=["GET"])
-def show_completed_hike_results():
-    """Shows all hike results"""
+    # this should occur when user selects a trail from the search trail page,
+    # user should redirect to the new hike info after added to db
+    
+    print("add a hike")
+    return redirect('/trails')
 
-    pass
+# @app.route("/results", methods=["GET"])
+# def show_completed_hike_results():
+#     """Shows all hike results"""
 
-@app.route("/results", methods=["POST"])
-def add_hike_result():
-    """Posts any changes to or adds a hike result for a completed hike"""
+#     pass
 
-    pass
+# @app.route("/results", methods=["POST"])
+# def add_hike_result():
+#     """Posts any changes to or adds a hike result for a completed hike"""
+
+#     pass
+
+@app.route("/goals", methods=["GET"])
+def show_current_goals_and_progress():
+    """View active goals and see graphs for current progress towards the goal"""
+    if not 'current_user' in session:
+        return redirect('/')
+
+    user_id = session["current_user"]
+
+    return render_template("goals.html")
+
+
+# @app.route("/goals", methods=["POST"])
+# def change_goals():
+#     """Add, modify, or cancel a selected, active goal"""
+
+#     pass
+
+# @app.route("/edit_goal/{goal_id}", methods=["GET"])
+# def modify_goal():
+#     """Modify a selected goal """
+
+# #  add similar route for submitting changes
+
+@app.route("/check_status", methods=["GET"])
+def check_current_trail_status():
+    """Checks for the current status of a trail"""
+# maybe I want to rethink my model in this respect as pulling trail data 
 
 @app.route("/about")
 def show_about_page():
