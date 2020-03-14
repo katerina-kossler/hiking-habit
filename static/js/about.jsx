0f26b7f095fd80ce6fd8ea5aec9c2784eb48670a
Katerina Kossler <katerinakossler@gmail.com>
2020-03-13 18:31:20 -0700

    reformatting of about page

--- static/js/about.jsx
+++ static/js/about.jsx
@@ -7,7 +7,7 @@ class About extends React.Component {
   render() {
     return(
       <div>
-        <div class="justify-content-center align-items-center ">
+        <div class="justify-content-center align-items-center">
           <div class="row justify-content-center align-items-center">
             <h3 class="text-center">A habit app</h3>
           </div>
@@ -15,16 +15,23 @@ class About extends React.Component {
             <h4 class="text-center">for the  thru-hiker, weekend walker, and everyone in between</h4>
           </div>
         </div>
-        <div class="row justify-content-center align-items-center">
-          <div class="p-3 text-center">
-            <h4>
+        <hr/>
+        <div class="justify-content-center align-items-center">
+          <div class="row justify-content-center align-items-center">
+            <h5 class="text-center">
               <a href="https://www.hikingproject.com/">The Hiking Project</a>
                   : REI's The Hiking Project was used to supply trail data.
-            </h4>
-            <h5>
+            </h5>
+          </div>
+          <div class="row justify-content-center align-items-center">
+            <h5 class="text-center">
               This project was inspired by my love of hiking and is my first full-stack project.
             </h5>
-            <h5>Check out the project's <a href="https://github.com/katerina-kossler/hiking-habit">GitHub</a> for more information</h5>
+          </div>
+          <div class="row justify-content-center align-items-center">
+            <h5 class="text-center">
+              Check out the project's <a href="https://github.com/katerina-kossler/hiking-habit">GitHub</a> for more information
+            </h5>
           </div>
         </div>
         <div class="row justify-content-center align-items-center p-3">
